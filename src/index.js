66240b80d455ad104ee031cfd4c93b381431fabc
Daniel Cunha de Oliveira <daniel.oliveira@coopersystem.com.br>
2022-07-19 13:46:56 -0300

    ✨ New features.
    Co-authored-by: name fcalldas@gmail.com

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 const { Discord, Intents } = require("discord.js")
 const dotenv = require("dotenv")
 
+dotenv.config();
+
+const client = new Discord.client({
+    intents : [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.login(process.env.TOKEN);
+
+client.on("ready", () =>{
+    console.log("Bot is ready!!!")
+});
+
